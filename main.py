604f8b0a8432b7f2dcbc8931052ce591ae749b91
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-11-28 15:33:27 +0530

    python modules library rename

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import my_module
 import math;
+import math as m;
+
 
 
 print("pi value :",math.pi)
+print("pi value :",m.pi)
 
 print("Factorial of 5 is :",math.factorial(4))
+print("Factorial of 5 is :",m.factorial(4))
+
 
 print(my_module.add(3,5))
